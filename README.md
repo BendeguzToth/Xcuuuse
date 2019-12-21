85ac9db3d329677419d71398b213ba4fab344c2a
BendeguzToth <bendeguz.toth@student.hu.nl>
2019-12-21 21:16:12 +0100

    Added basic data type + grammar specs

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Xcuuuse
 Proof checker
+
+how to build: `ghc --make -shared -fPIC Xcuuuse.hs -o xcuuus.so`
+
+## Grammar
+
+*Formula* ::= *Variable* | *Negation* | *Conjunction* | *Disjunction* | *Implication* | *Equivalence*  
+*Variable* ::= *Uppercase* | *Variable* *Num* | *Variable* '  
+*Negation* ::= *Negsym* *Formula*  
+*Conjunction* ::= *Formula* *Consym* *Formula*  
+*Disjunction* ::= *Formula* *Dissym* *Formula*  
+*Implication* ::= *Formula* *Impsym* *Formula*  
+*Equivalence* ::= *Formula* *Eqsym* *Formula*  
+
+*Num* ::= 0|...|9  
+*Uppercase* ::= A|...|Z  
+*Negsym* ::= - | ~  
+*Consym* ::= & | /\  
+*Dissym* ::= v | \\/  
+*Impsym* ::= -> | =>  
+*Eqsym* ::= = | <=>  
---
+++ src/Xcuuuse.hs
@@ -0,0 +1,44 @@
+import Prelude
+
+data Formula =  Var String 
+                | Negation Formula 
+                | Conjunction Formula Formula
+                | Disjunction Formula Formula
+                | Implication Formula Formula
+                | Equivalence Formula Formula
+
+instance Eq Formula where
+    (Var s1) == (Var s2) = s1 == s2
+    (Negation f1) == (Negation f2) = f1 == f2
+    (Conjunction f1 f2) == (Conjunction e1 e2)  | f1 == e1 && f2 == e2 = True
+                                                | f1 == e2 && f2 == e1 = True
+                                                | otherwise = False
+    (Disjunction f1 f2) == (Conjunction e1 e2)  | f1 == e1 && f2 == e2 = True
+                                                | f1 == e2 && f2 == e1 = True
+                                                | otherwise = False
+    (Implication p1 c1) == (Implication p2 c2) = p1 == p2 && c1 == c2
+    (Equivalence f1 f2) == (Equivalence e1 e2) = f1 == e1 && f2 == e2
+    _ == _ = False
+
+instance Show Formula where
+    show (Var s) = s
+    show (Negation f) = "-" ++ (show f)
+    show (Conjunction f1 f2) = (show f1) ++ "/\\" ++ (show f2)
+    show (Disjunction f1 f2) = (show f1) ++ "\\/" ++ (show f2)
+    show (Implication f1 f2) = (show f1) ++ "=>" ++ (show f2)
+    show (Equivalence f1 f2) = (show f1) ++ "<=>" ++ (show f2)
+
+
+conjunctionIntroduction :: Formula -> Formula -> Formula -> Bool
+conjunctionIntroduction p1 p2 f = Conjunction p1 p2 == f
+
+conjunctionElimination :: Formula -> Formula -> Bool
+conjunctionElimination (Conjunction p1 p2) c = c == p1 || c == p2
+conjunctionElimination _ _ = False
+
+doubleNegationIntroduction :: Formula -> Formula -> Bool
+doubleNegationIntroduction p f = Negation (Negation p) == f
+
+doubleNegationElimination :: Formula -> Formula -> Bool
+doubleNegationElimination (Negation (Negation p)) f = f == p
+doubleNegationElimination _ _ = False
